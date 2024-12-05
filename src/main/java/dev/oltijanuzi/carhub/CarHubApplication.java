7ff4bca94f1ad3a8f77dc04e84bf9fb3d3496cdd
OltiG <oltii.januzii@gmail.com>
2024-12-05 18:08:41 +0100

    Commit para strukturimit te projektit

---
+++ src/main/java/dev/oltijanuzi/carhub/CarHubApplication.java
@@ -0,0 +1,13 @@
+package dev.oltijanuzi.carhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarHubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarHubApplication.class, args);
+    }
+
+}
